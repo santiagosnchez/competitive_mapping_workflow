89dd75fa9678ca7d015300a3d4177509d9fce962
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2020-09-01 15:56:26 -0400

    updated analyses, r scripts, tables, and figs

--- analyses/scripts/Fig1_data_new.R
+++ analyses/scripts/Fig1_data_new.R
@@ -105,58 +105,165 @@ write.csv(df.sex.count.chr, file="tables/DE_per_sex_chr_enrichment.csv", row.nam
 
 # expression divergence
 # females
+# between species
 keep = (species == "Cbr" | species == "Cni") & sex == "F"
-dds.briggsae.vs.nigoni = DESeqDataSetFromMatrix(countData = counts(dds)[,keep], colData = coldata[keep,], design = ~ species)
-dds.briggsae.vs.nigoni = DESeq(dds.briggsae.vs.nigoni)
-dds.briggsae.vs.nigoni.res = results(dds.briggsae.vs.nigoni)
-write.csv(as.data.frame(dds.briggsae.vs.nigoni.res), file="tables/deseq_res.briggsae.vs.nigoni.sex.csv", quote=F)
-dds.briggsae.vs.nigoni.sum = as.data.frame(dds.briggsae.vs.nigoni.res) %>% mutate(CniF = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>% select(Cni)
+dds.briggsae.vs.nigoni.F = DESeqDataSetFromMatrix(countData = counts(dds)[,keep], colData = coldata[keep,], design = ~ species)
+dds.briggsae.vs.nigoni.F = DESeq(dds.briggsae.vs.nigoni.F)
+dds.briggsae.vs.nigoni.res.F = results(dds.briggsae.vs.nigoni.F)
+dds.briggsae.vs.nigoni.sum.F = as.data.frame(dds.briggsae.vs.nigoni.res.F) %>%
+    mutate(CniF = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>%
+    select(CniF)
+dds.briggsae.vs.nigoni.res.F = cbind(as.data.frame(dds.briggsae.vs.nigoni.res.F),dds.briggsae.vs.nigoni.sum.F)
+write.csv(dds.briggsae.vs.nigoni.res.F, file="tables/deseq_res.briggsae.vs.nigoni.sex.F.csv", quote=F)
+
+# hybrids vs briggsae
+keep = (species == "Cbr" | species == "HF1") & sex == "F"
+dds.briggsae.vs.hybrids.F = DESeqDataSetFromMatrix(countData = counts(dds)[,keep], colData = coldata[keep,], design = ~ species)
+dds.briggsae.vs.hybrids.F = DESeq(dds.briggsae.vs.hybrids.F)
+dds.briggsae.vs.hybrids.res.F = results(dds.briggsae.vs.hybrids.F)
+dds.briggsae.vs.hybrids.sum.F = as.data.frame(dds.briggsae.vs.hybrids.res.F) %>%
+    mutate(HF1F = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>%
+    select(HF1F)
+dds.briggsae.vs.hybrids.res.F = cbind(as.data.frame(dds.briggsae.vs.hybrids.res.F),dds.briggsae.vs.hybrids.sum.F)
+write.csv(dds.briggsae.vs.hybrids.res.F, file="tables/deseq_res.briggsae.vs.hybrids.sex.F.csv", quote=F)
+
+# hybrids vs nigoni
+keep = (species == "Cni" | species == "HF1") & sex == "F"
+dds.nigoni.vs.hybrids.F = DESeqDataSetFromMatrix(countData = counts(dds)[,keep], colData = coldata[keep,], design = ~ species)
+dds.nigoni.vs.hybrids.F = DESeq(dds.nigoni.vs.hybrids.F)
+dds.nigoni.vs.hybrids.res.F = results(dds.nigoni.vs.hybrids.F)
+dds.nigoni.vs.hybrids.sum.F = as.data.frame(dds.nigoni.vs.hybrids.res.F) %>%
+    mutate(HF1F = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>%
+    select(HF1F)
+dds.nigoni.vs.hybrids.res.F = cbind(as.data.frame(dds.nigoni.vs.hybrids.res.F),dds.nigoni.vs.hybrids.sum.F)
+write.csv(dds.nigoni.vs.hybrids.res.F, file="tables/deseq_res.nigoni.vs.hybrids.sex.F.csv", quote=F)
 
 # males
-e.briggsae.vs.nigoni.male = e$counts[,c(4:6,16:18)]
-e.briggsae.vs.nigoni.male = DGEList(e.briggsae.vs.nigoni.male)
-e.briggsae.vs.nigoni.male$samples$group = factor(groups[c(4:6,16:18)])
-e.briggsae.vs.nigoni.male = calcNormFactors(e.briggsae.vs.nigoni.male, method = "TMM")
-e.briggsae.vs.nigoni.male = estimateCommonDisp(e.briggsae.vs.nigoni.male)
-e.briggsae.vs.nigoni.male = estimateGLMTrendedDisp(e.briggsae.vs.nigoni.male)
-e.briggsae.vs.nigoni.male = estimateTagwiseDisp(e.briggsae.vs.nigoni.male)
-v.briggsae.vs.nigoni.male = voom(e.briggsae.vs.nigoni.male, design.species, plot=F)
-vfit.briggsae.vs.nigoni.male = lmFit(v.briggsae.vs.nigoni.male, design.species)
-vfit.briggsae.vs.nigoni.male = eBayes(vfit.briggsae.vs.nigoni.male)
-dt.briggsae.vs.nigoni.male = decideTests(vfit.briggsae.vs.nigoni.male, p.value = 0.05)
-tt.briggsae.vs.nigoni.male = topTable(vfit.briggsae.vs.nigoni.male, n = Inf)
-tt.briggsae.vs.nigoni.male = tt.briggsae.vs.nigoni.male[ rownames(dt.briggsae.vs.nigoni.male), ]
-write.csv(tt.briggsae.vs.nigoni.male, file="tt.briggsae.vs.nigoni.males.csv", quote=F)
+# between species
+keep = (species == "Cbr" | species == "Cni") & sex == "M"
+dds.briggsae.vs.nigoni.M = DESeqDataSetFromMatrix(countData = counts(dds)[,keep], colData = coldata[keep,], design = ~ species)
+dds.briggsae.vs.nigoni.M = DESeq(dds.briggsae.vs.nigoni.M)
+dds.briggsae.vs.nigoni.res.M = results(dds.briggsae.vs.nigoni.M)
+dds.briggsae.vs.nigoni.sum.M = as.data.frame(dds.briggsae.vs.nigoni.res.M) %>%
+    mutate(CniM = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>%
+    select(CniM)
+dds.briggsae.vs.nigoni.res.M = cbind(as.data.frame(dds.briggsae.vs.nigoni.res.M),dds.briggsae.vs.nigoni.sum.M)
+write.csv(dds.briggsae.vs.nigoni.res.M, file="tables/deseq_res.briggsae.vs.nigoni.sex.M.csv", quote=F)
+
+# hybrids vs briggsae
+keep = (species == "Cbr" | species == "HF1") & sex == "M"
+dds.briggsae.vs.hybrids.M = DESeqDataSetFromMatrix(countData = counts(dds)[,keep], colData = coldata[keep,], design = ~ species)
+dds.briggsae.vs.hybrids.M = DESeq(dds.briggsae.vs.hybrids.M)
+dds.briggsae.vs.hybrids.res.M = results(dds.briggsae.vs.hybrids.M)
+dds.briggsae.vs.hybrids.sum.M = as.data.frame(dds.briggsae.vs.hybrids.res.M) %>%
+    mutate(HF1M = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>%
+    select(HF1M)
+dds.briggsae.vs.hybrids.res.M = cbind(as.data.frame(dds.briggsae.vs.hybrids.res.M),dds.briggsae.vs.hybrids.sum.M)
+write.csv(dds.briggsae.vs.hybrids.res.M, file="tables/deseq_res.briggsae.vs.hybrids.sex.M.csv", quote=F)
+
+# hybrids vs nigoni
+keep = (species == "Cni" | species == "HF1") & sex == "M"
+dds.nigoni.vs.hybrids.M = DESeqDataSetFromMatrix(countData = counts(dds)[,keep], colData = coldata[keep,], design = ~ species)
+dds.nigoni.vs.hybrids.M = DESeq(dds.nigoni.vs.hybrids.M)
+dds.nigoni.vs.hybrids.res.M = results(dds.nigoni.vs.hybrids.M)
+dds.nigoni.vs.hybrids.sum.M = as.data.frame(dds.nigoni.vs.hybrids.res.M) %>%
+    mutate(HF1M = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>%
+    select(HF1M)
+dds.nigoni.vs.hybrids.res.M = cbind(as.data.frame(dds.nigoni.vs.hybrids.res.M),dds.nigoni.vs.hybrids.sum.M)
+write.csv(dds.nigoni.vs.hybrids.res.M, file="tables/deseq_res.nigoni.vs.hybrids.sex.M.csv", quote=F)
 
 # consolidate
 # expression divergence per sex
-df.exprdiv = rbind(data.frame(logFC=tt.briggsae.vs.nigoni.male[,1],
-                              DE=c("sig","no sig","sig")[ factor(dt.briggsae.vs.nigoni.male[,2]) ],
-                              species=c("nigoni","briggsae")[ factor(tt.briggsae.vs.nigoni.male[,1] < 0) ],
+df.exprdiv = rbind(data.frame(logFC=dds.briggsae.vs.nigoni.res.M$log2FoldChange,
+                              DE=c("sig","no sig","sig")[ factor(dds.briggsae.vs.nigoni.sum.M[,1]) ],
+                              species=c("briggsae","nigoni")[ factor(dds.briggsae.vs.nigoni.res.M$log2FoldChange > 0) ],
                               sex="male",
-                              p.value=tt.briggsae.vs.nigoni.male[,"adj.P.Val"],
-                              genes=rownames(tt.briggsae.vs.nigoni.male)),
-                  data.frame(logFC=tt.briggsae.vs.nigoni.female[,1],
-                              DE=c("sig","no sig","sig")[ factor(dt.briggsae.vs.nigoni.female[,2]) ],
-                              species=c("nigoni","briggsae")[ factor(tt.briggsae.vs.nigoni.female[,1] < 0) ],
+                              p.value=dds.briggsae.vs.nigoni.res.M$padj,
+                              genes=rownames(dds.briggsae.vs.nigoni.res.M), stringsAsFactors=F),
+                  data.frame(logFC=dds.briggsae.vs.nigoni.res.F$log2FoldChange,
+                              DE=c("sig","no sig","sig")[ factor(dds.briggsae.vs.nigoni.sum.F[,1]) ],
+                              species=c("briggsae","nigoni")[ factor(dds.briggsae.vs.nigoni.res.F$log2FoldChange > 0) ],
                               sex="female",
-                              p.value=tt.briggsae.vs.nigoni.female[,"adj.P.Val"],
-                              genes=rownames(tt.briggsae.vs.nigoni.female)))
+                              p.value=dds.briggsae.vs.nigoni.res.F$padj,
+                              genes=rownames(dds.briggsae.vs.nigoni.res.F), stringsAsFactors=F))
 df.exprdiv$chromosome = sub("\\..*","",df.exprdiv$genes)
+df.exprdiv = na.omit(df.exprdiv)
 df.exprdiv$species_DE = paste(df.exprdiv$species, df.exprdiv$DE)
-write.csv(df.exprdiv, file="df.expr_div.per_sex.csv", row.names=F)
+
+write.csv(df.exprdiv, file="tables/df.expr_div.per_sex.csv", row.names=F)
 
 # DE expression enrichment per sex
 df.exprdiv2 = df.exprdiv
 df.exprdiv2$species_DE[ grep("no sig", df.exprdiv2$species_DE) ] = "no sig"
-df.DE.enrich = as.data.frame(df.exprdiv2 %>% group_by(sex, species_DE, chromosome) %>% count())
-df.DE.enrich$p.value = NA
-df.DE.enrich$enrichment = NA
-x = matrix((df.DE.enrich %>% filter(sex == "male"))$n, ncol=3, nrow=6)
-df.DE.enrich$p.value[1:18] = as.vector(enrichment(x))
-df.DE.enrich$enrichment[1:18] = as.vector(enrichment(x, odds.ratio=T))
-x = matrix((df.DE.enrich %>% filter(sex == "female"))$n, ncol=3, nrow=6)
-df.DE.enrich$p.value[19:36] = as.vector(enrichment(x))
-df.DE.enrich$enrichment[19:36] = as.vector(enrichment(x, odds.ratio=T))
-df.DE.enrich$chr.pseudo = (1:6)[ factor(df.DE.enrich$chromosome) ]
-write.csv(df.DE.enrich, file="df.enrichment.DE.sex.csv", row.names=F)
+df.DE.exprdiv.enrich = as.data.frame(df.exprdiv2 %>% group_by(sex, species_DE, chromosome) %>% summarise(count=n()))
+df.DE.exprdiv.enrich$p.value = NA
+df.DE.exprdiv.enrich$enrichment = NA
+x = matrix((df.DE.exprdiv.enrich %>% filter(sex == "male"))$count, ncol=3, nrow=6)
+df.DE.exprdiv.enrich$p.value[1:18] = as.vector(enrichment(x))
+df.DE.exprdiv.enrich$enrichment[1:18] = as.vector(enrichment(x, odds.ratio=T))
+x = matrix((df.DE.exprdiv.enrich %>% filter(sex == "female"))$count, ncol=3, nrow=6)
+df.DE.exprdiv.enrich$p.value[19:36] = as.vector(enrichment(x))
+df.DE.exprdiv.enrich$enrichment[19:36] = as.vector(enrichment(x, odds.ratio=T))
+df.DE.exprdiv.enrich$chr.pseudo = (1:6)[ factor(df.DE.exprdiv.enrich$chromosome) ]
+write.csv(df.DE.exprdiv.enrich, file="tables/df.enrichment.exprdiv.DE.sex.csv", row.names=F)
+
+# allele-specific expression cis divergence
+# prep data
+counts.ase.filter = counts.ase[filter_in, c(grep("Cbr", colnames(counts.ase)), grep("Cni", colnames(counts.ase)))]
+keep = species == "Cbr" | species == "Cni"
+coldata.ase = coldata[keep,]
+coldata.ase$samples = colnames(counts.ase)
+
+# females
+keep = coldata.ase$sex == "F"
+dds.ase.F = DESeqDataSetFromMatrix(countData = counts.ase.filter[,keep], colData = coldata.ase[keep,], design = ~ species)
+dds.ase.F = DESeq(dds.ase.F)
+dds.ase.res.F = results(dds.ase.F)
+dds.ase.sum.F = as.data.frame(dds.ase.res.F) %>% mutate(CniF = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>% select(CniF)
+dds.ase.res.F = cbind(as.data.frame(dds.ase.res.F),dds.ase.sum.F)
+write.csv(dds.ase.res.F, file="tables/deseq_res.ase.females.csv", quote=F)
+
+
+# males
+keep = coldata.ase$sex == "M"
+auto = rownames(counts.ase.filter)[ grep('^X', rownames(counts.ase.filter), invert=T)]
+dds.ase.M = DESeqDataSetFromMatrix(countData = counts.ase.filter[auto,keep], colData = coldata.ase[keep,], design = ~ species)
+dds.ase.M = DESeq(dds.ase.M)
+dds.ase.res.M = results(dds.ase.M)
+dds.ase.sum.M = as.data.frame(dds.ase.res.M) %>% mutate(CniM = ifelse(padj > 0.05, 0, ifelse(log2FoldChange > 0, 1, -1))) %>% select(CniM)
+dds.ase.res.M = cbind(as.data.frame(dds.ase.res.M),dds.ase.sum.M)
+write.csv(dds.ase.res.M, file="tables/deseq_res.ase.males.csv", quote=F)
+
+# consolidate ASE
+df.ase = rbind(data.frame(logFC=dds.ase.res.M$log2FoldChange,
+                              DE=c("sig","no sig","sig")[ factor(dds.ase.sum.M[,1]) ],
+                              species=c("briggsae","nigoni")[ factor(dds.ase.res.M$log2FoldChange > 0) ],
+                              sex="male",
+                              p.value=dds.ase.res.M$padj,
+                              genes=rownames(dds.ase.res.M), stringsAsFactors=F),
+                  data.frame(logFC=dds.ase.res.F$log2FoldChange,
+                              DE=c("sig","no sig","sig")[ factor(dds.ase.sum.F[,1]) ],
+                              species=c("briggsae","nigoni")[ factor(dds.ase.res.F$log2FoldChange > 0) ],
+                              sex="female",
+                              p.value=dds.ase.res.F$padj,
+                              genes=rownames(dds.ase.res.F), stringsAsFactors=F))
+df.ase$chromosome = sub("\\..*","",df.ase$genes)
+df.ase = na.omit(df.ase)
+df.ase$species_DE = paste(df.ase$species, df.ase$DE)
+
+write.csv(df.ase, file="tables/df.ase.per_sex.csv", row.names=F)
+
+# ASE enrichment per sex
+df.ase2 = df.ase
+df.ase2$species_DE[ grep("no sig", df.ase2$species_DE) ] = "no sig"
+df.DE.ase.enrich = as.data.frame(df.ase2 %>% group_by(sex, species_DE, chromosome) %>% summarise(count=n()))
+df.DE.ase.enrich$p.value = NA
+df.DE.ase.enrich$enrichment = NA
+x = matrix((df.DE.ase.enrich %>% filter(sex == "female"))$count, ncol=3, nrow=6)
+df.DE.ase.enrich$p.value[1:18] = as.vector(enrichment(x))
+df.DE.ase.enrich$enrichment[1:18] = as.vector(enrichment(x, odds.ratio=T))
+x = matrix((df.DE.ase.enrich %>% filter(sex == "male"))$count, ncol=3, nrow=5)
+df.DE.ase.enrich$p.value[19:33] = as.vector(enrichment(x))
+df.DE.ase.enrich$enrichment[19:33] = as.vector(enrichment(x, odds.ratio=T))
+df.DE.ase.enrich$chr.pseudo = (1:6)[ factor(df.DE.ase.enrich$chromosome) ]
+write.csv(df.DE.ase.enrich, file="tables/df.enrichment.ase.DE.sex.csv", row.names=F)
